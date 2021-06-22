28d38a1f21a1fda948f0975156a9ad953765db52
Graeme Foster <graeme@codeforbreakfast.co>
2021-06-22 19:04:58 +0100

    Log the lot

--- src/main.ts
+++ src/main.ts
@@ -7,4 +7,11 @@ const notificationStrings = fromEvent(new Bluetooth(), 'data').pipe(
   map((data) => data.toString()),
 );
 
-grainfatherNotifications(notificationStrings).subscribe((x) => console.log(x));
+const notifications = grainfatherNotifications(notificationStrings);
+
+notifications.temperature$.subscribe((x) => console.log(x));
+notifications.config$.subscribe((x) => console.log(x));
+notifications.misc$.subscribe((x) => console.log(x));
+notifications.status$.subscribe((x) => console.log(x));
+notifications.timer$.subscribe((x) => console.log(x));
+notifications.interaction$.subscribe((x) => console.log(x));
